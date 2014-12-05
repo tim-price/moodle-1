5ce3c2eb7934b382557466691086d3f01bef1bf4
Tim Price <timprice@catalyst-au.net>
2014-12-05 13:09:25 +1100

    MDL-18177 backup/moodle2: fixed comment

--- backup/moodle2/backup_stepslib.php
+++ backup/moodle2/backup_stepslib.php
@@ -1145,7 +1145,7 @@ class backup_groups_structure_step extends backup_structure_step {
                    AND bi.itemname = 'groupingfinal'", array(backup::VAR_BACKUPID));
             $groupinggroup->set_source_table('groupings_groups', array('groupingid' => backup::VAR_PARENTID));
 
-            // This only happens if we are including users
+            // This only happens if we are including users.
             if ($userinfo) {
                 $member->set_source_table('groups_members', array('groupid' => backup::VAR_PARENTID));
             }

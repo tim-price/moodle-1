542c0a3eb34629367c6f355330c9fdb385e5c095
Tim Price <timprice@catalyst-au.net>
2014-12-05 13:12:21 +1100

    MDL-18177: removing unwanted whitespace

--- backup/moodle2/backup_stepslib.php
+++ backup/moodle2/backup_stepslib.php
@@ -1098,7 +1098,7 @@ class backup_groups_structure_step extends backup_structure_step {
         $groupinfo = $this->get_setting_value('groups');
 
         // Define each element separate.
-        
+
         $groups = new backup_nested_element('groups');
 
         $group = new backup_nested_element('group', array('id'), array(
@@ -1152,7 +1152,7 @@ class backup_groups_structure_step extends backup_structure_step {
                    AND bi.itemname = 'groupingfinal'", array(backup::VAR_BACKUPID));
             $groupinggroup->set_source_table('groupings_groups', array('groupingid' => backup::VAR_PARENTID));
 
-            // This only happens if we are including users
+            // This only happens if we are including users.
             if ($userinfo) {
                 $member->set_source_table('groups_members', array('groupid' => backup::VAR_PARENTID));
             }
